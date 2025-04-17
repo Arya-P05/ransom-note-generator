f7f9db6579bfe7ae13bfb3912c457ac719e140c3
Arya <jackrack040@gmail.com>
2025-04-17 11:08:16 -0400

    letterdata an state

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ import html2canvas from "html2canvas";
 export default function Home() {
   const [text, setText] = useState("");
   const [generated, setGenerated] = useState<ReactNode[][]>([]);
+  type LetterData = {
+    char: string;
+    variation: number;
+  };
+
+  const [letters, setLetters] = useState<LetterData[]>([]);
 
   const VARIATION_COUNTS: { [key: string]: number } = {
     "0": 18,
